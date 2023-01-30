966a310459892f188d93ea14ef2dffb04e0c7c36
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2023-01-30 07:31:39 -0500

    must add logic to determine winner

--- js/index.js
+++ js/index.js
@@ -1,29 +1,48 @@
 const Gameboard = (() => {
-    const newGame = document.querySelector("new-btn");
-    const restartGame = document.querySelector(".restart-game");
-    const cell = document.querySelectorAll(".cell")
-    cell.forEach(cells => {
-        cells.addEventListener("click", handleClickCell)
+    const gameboard = ["", "", "", "", "", "", "", "", ""];
+    const playerInformation = (name, marker) => {
+        return {name, marker}
+    }
+    const playerOne = playerInformation("player-1", "X");
+    const playerTwo = playerInformation("player-2", "O");
+    let currentPlayer = playerOne.marker
+    // grab current board
+    const boards = document.querySelectorAll(".cell");
+    const winningCondition = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function checkWinner() {
+        for(let i =0; i < winningCondition.length; i++) {
+            if(winningCondition[i] === playerOne.marker) {
+                gameboard[0]
+            } else if(winningCondition[i] === playerTwo.marker) {
+                gameboard[1]
+            } else {
+                // It's a tie
+            }
+        }
+    }
+    boards.forEach((board) => {
+        board.addEventListener("click", () => {
+            if(board.textContent === "") {
+                board.textContent = currentPlayer;
+                if(currentPlayer === playerOne.marker) {
+                    currentPlayer = playerTwo.marker
+                } else if(currentPlayer === playerTwo.marker) {
+                    currentPlayer = playerOne.marker
+                }
+            }
+            if(board.textContent !== "") {
+                preventDefault()
+            }
+        })
     })
-    let gameActive = true;
-    const playerCircle = document.querySelector(".player-circle")
-    const boards = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-    const playerInformation = (name, marker, turn) => {
-        return {name, marker, turn}
-    }
-    const player1 = playerInformation("player1", "x", true);
-    const player2 = playerInformation("player2", "o", false );
-
-    const currentPlayer = player1.marker;
-
-    function handleClickCell(clickedCellEvent) {
-        const clickedCell = clickedCellEvent.target
-
-        const cellClickedIndex = parseInt(
-            clickedCell.getAttribute('data-cell-index')
-          );
-          if(boards[cellClickedIndex] != " " || !gameActive) {
-            return true;
-          }
-    }
 })();
